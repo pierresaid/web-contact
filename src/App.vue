78df747938e5fe64de1309f090b54cc331c0e8b6
pierre said <pierre.said@epitech.eu>
2020-02-21 14:25:57 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <button @click="onClick">Test Contacts</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  methods: {
+    onClick() {
+      if (navigator.contacts) {
+        alert("Yes");
+      } else {
+        alert("No");
+      }
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
